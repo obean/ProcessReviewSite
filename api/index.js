66437f0a063542658d32393e0267f74ca074025e
Johnny Yip <jyippy2012@gmail.com>
2020-11-11 09:51:54 +0000

    Okta index

---
+++ api/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('public'));
+
+// @TODO add auth middleware
+app.use(
+    require('express-session')({
+      secret: process.env.APP_SECRET,
+      resave: true,
+      saveUninitialized: false
+    })
+  );
+  
+  const { ExpressOIDC } = require('@okta/oidc-middleware');
+  const oidc = new ExpressOIDC({
+    appBaseUrl: process.env.HOST_URL,
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+    client_id: process.env.OKTA_CLIENT_ID,
+    client_secret: process.env.OKTA_CLIENT_SECRET,
+    redirect_uri: `${process.env.HOST_URL}/callback`,
+    scope: 'openid profile',
+    routes: {
+      loginCallback: {
+        path: '/callback'
+      },
+    }
+  });
+  
+  app.use(oidc.router);
+
+// @TODO add registration page
+
+app.use('/', require('./routes/index'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`));
