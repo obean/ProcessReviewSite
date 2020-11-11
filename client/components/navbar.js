0028354a2931b483c8baaebdcad110f40ab02b57
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-11 14:55:29 +0000

    working on the navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,13 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, NavItem, NavDropdown, MenuItem, Nav } from 'react-bootstrap';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Navbar bg="light" expand="lg">
+<Navbar bg="light" expand="lg">
   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
@@ -22,28 +13,9 @@ function App() {
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
-    {/* <Form inline>
+    <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-success">Search</Button>
-    </Form> */}
+    </Form>
   </Navbar.Collapse>
 </Navbar>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
