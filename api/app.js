356f8b2a0f9d09d624383dd370a7eafe5a002176
ollie beney <ombeney@gmail.com>
2020-11-12 07:16:02 +0000

    server accepts post request to add user to the database

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-
+const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -17,9 +17,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- api/routes/users.js
+++ api/routes/users.js
@@ -4,6 +4,7 @@ var  models  = require('../models');
 
 
 
+
 // Start express, Sequelize generates models, established DB connection
 // Then Models are ready to be used within routes
 
@@ -17,9 +18,14 @@ router.get('/', async function(req, res, next) {
   res.send(JSON.stringify(users, null, 2));
 });
 
-router.post('/sign-up', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/new', async function(req, res, next) {
+  // console.log(req.body + 'req')
+ 
+  console.log(req.body)
+  const user = await models.User.create({ firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email });
+  console.log(user);  ;
 });
+
 
 // router.get('/sign-up', function(req, res, next) {
 //   res.send('respond with a resource');
