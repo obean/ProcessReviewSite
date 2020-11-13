f093a155698dc2102ee7a67c056c90c58c30b55e
courtenay donald <co.donald18@gmail.com>
2020-11-13 11:36:31 +0000

    break through with grid box on the profile js

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,5 +1,6 @@
 import React, { Component, useState, useEffect } from "react";
-
+import '../profile.css';
+import Login from "./components/login.component";
 
 export default class Profile extends Component {
 
@@ -19,19 +20,19 @@ export default class Profile extends Component {
   //     console.log(data);
   //   };
   // }
-    render() {
-        return (
-          <div className="Profile">
-          <h1>  hello world</h1>
+  render() {
+    return (
+      <div className="Profile">
+        <h1>  hello world</h1>
 
-            <div class="grid-container">
-              <div class="grid-item">Line chart</div>
-              <div class="grid-item">Reviews list</div>
-              <div class="grid-item">Radar chart</div>
-              <div class="grid-item">TBC</div>
-            </div>
+        <div class="grid-container">
+          <div class="grid-item"> <Login /> </div>
+          <div class="grid-item">Reviews list</div>
+          <div class="grid-item">Radar chart</div>
+          <div class="grid-item">TBC</div>
+        </div>
 
-          </div>
-        );
-    };
+      </div>
+    );
+  };
 };
