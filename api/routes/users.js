c84a1b0e9e362bac63058badf31ad44791e07693
ollie beney <ombeney@gmail.com>
2020-11-14 15:02:46 +0000

    upates user route to use passport and install packages

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,6 +4,74 @@ var  models  = require('../models');
 const bcrypt = require('bcrypt')
 
 
+// local strategy
+module.exports = ( passport) => {
+  //serialize use
+  passport.serializeUser(function(user, cb) {
+    cb(null, {username: user.username});
+  });
+
+  //deserialize
+  passport.deserializeUser(function(id, cb) {
+    models.User.findById(id, function (err, user) {
+      if (err) { return cb(err); }
+      cb(null, user);
+    });
+  });
+ 
+  var LocalStrategy = require('passport-local').Strategy;
+  passport.use(new LocalStrategy(
+    async function(username, password, cb) {
+      // console.log(username, password)
+        try {
+             const user = await models.User.findOne({
+              where: { username: username}
+            })
+            if(!user) {
+              console.log("not found!")
+              return cb(null, false)
+            }  
+            if(!bcrypt.compareSync(password, user.password)){
+              console.log("incorrect password")
+              return cb(null, false)
+            }
+            return cb(null, user)
+
+        } catch (err) {
+            console.log(err)
+            return cb(err)
+        }
+  }))
+
+  //verify login via local strategy
+  router.post('/login',
+    passport.authenticate('local' ),//{ failureRedirect: '/login' }
+      function(req, res) {
+        // console.log(res)
+        res.send(JSON.stringify(200));
+        //console.log(typeof req)
+      }
+  );
+
+  router.get('/logged-in', loggedIn, function(req, res) {
+    console.log(isAuthenticated)
+    console.log(req.user)
+    console.log(req.body)
+  })
+
+  function loggedIn(req, res, next) {
+    if( req.isAuthenticated()) {
+        
+      console.log("true")
+      return next()
+    } else {
+      console.log("not authenticated")
+    }
+  }
+
+  return router
+
+}
 
 // Start express, Sequelize generates models, established DB connection
 // Then Models are ready to be used within routes
@@ -41,4 +109,4 @@ router.get('/', async function(req, res, next) {
 // router.get('/sign-up', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
-module.exports = router;
+//module.exports = router;
