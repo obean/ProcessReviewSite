a579c2cbf1524c1bcd3598aac63480e748c56529
ollie beney <ombeney@gmail.com>
2020-11-14 19:48:23 +0000

    added get request for current user to profile

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -4,7 +4,18 @@ import ReviewList from './reviewList.component'
 
 
 function Profile() {
+  useEffect(() => {
+    fetchUser();
+  }, [])
 
+  const fetchUser = async (res) => {
+    const data = await fetch('http://localhost:9000/users/logged-in');
+    const user = await data.json();
+    console.log(user)
+    setUser(user)
+  }
+  const [user, setUser] = useState([])
+  
     return (
       <div className="Profile">
         <h1>  hello world</h1>
