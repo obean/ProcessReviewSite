e2f960f8ac790ad582d42b2baf4ac15daabb167e
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-15 11:09:40 +0000

    Trying two charts on same row

--- client/src/components/lineChart.component.js
+++ client/src/components/lineChart.component.js
@@ -49,11 +49,11 @@ const RatingsList = () => {
       <Legend />
       <Line type="monotone" dataKey="TDD" stroke="#8884d8" activeDot={{ r: 8 }} />
       <Line type="monotone" dataKey="Fluency" stroke="#82ca8d" />
-      <Line type="monotone" dataKey="Debug" stroke="#82ca5d" />
-      <Line type="monotone" dataKey="Model" stroke="#82ca3d" />
-      <Line type="monotone" dataKey="Refactor" stroke="#82ca2d" />
-      <Line type="monotone" dataKey="Agile" stroke="#82ca1d" />
-      <Line type="monotone" dataKey="Maintainability" stroke="#82ca6d" />
+      <Line type="monotone" dataKey="Debug" stroke="#23ca5d" />
+      <Line type="monotone" dataKey="Model" stroke="#56ca3d" />
+      <Line type="monotone" dataKey="Refactor" stroke="#76ca2d" />
+      <Line type="monotone" dataKey="Agile" stroke="#45ca1d" />
+      <Line type="monotone" dataKey="Maintainability" stroke="#45ca6d" />
     </LineChart>
   );
 
