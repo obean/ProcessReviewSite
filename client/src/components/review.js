2de0c07486e8183ad2d3b0ec8ef86772405a42e3
courtenay <69038480+c-donald@users.noreply.github.com>
2020-11-16 17:59:30 +0000

    Review (#9)
    * set up api route
    
    * api route
    
    * review component is fetching api call
    showing the review data list
    added an api call in the routes
    
    * api call fixed
    added the date
    structured how the api prints out the review
    fixed the id to match the booking review

---
+++ client/src/components/review.js
@@ -0,0 +1,76 @@
+import React, { Component, useState, useEffect } from "react";
+import '../App.css';
+import ReviewList from './reviewList.component'
+import { Redirect, useParams } from 'react-router';
+
+
+function Review() {
+    useEffect(() => {
+        fetchReview();
+    }, []);
+
+    const [review, setReview] = useState({});
+    const { id } = useParams();
+    console.log(id)
+    const fetchReview = async () => {
+        const data = await fetch(`http://localhost:9000/reviews/get-review?id=${id}`);
+        const review = await data.json();
+        setReview(review)
+    }
+
+
+    return (
+        <div>
+            <h1> Review Page </h1>
+            <div id='BookingDate'>
+                <h2>d</h2>
+                <p> {review.booking_date}</p>
+            </div>
+
+            <div id='GeneralFeedback'>
+                <h2> General Feedback </h2>
+                <p> {review.general_feedback} </p>
+            </div>
+
+            <div id='TDD_description'>
+                <h2> TDD Description {review.TDD_rating}/100</h2>
+                <p> {review.TDD_description} </p>
+            </div>
+
+            <div id='Fluency_description'>
+                <h2> Fluency Description {review.Fluency_rating}/100 </h2>
+                <p> {review.Fluency_description} </p>
+            </div>
+
+            <div id='Debug_description'>
+                <h2> Debug Description {review.Debug_rating}/100 </h2>
+                <p> {review.Debug_description} </p>
+            </div>
+
+            <div id='Model_description'>
+                <h2> Model Description {review.Model_rating}/100 </h2>
+                <p> {review.Model_description} </p>
+            </div>
+
+            <div id='Refactor_description'>
+                <h2> Refactor Description {review.Refactor_rating}/100 </h2>
+                <p> {review.Refactor_description} </p>
+            </div>
+
+            <div id='Agile_description'>
+                <h2> Agile Description {review.Agile_rating}/100 </h2>
+                <p> {review.Agile_description} </p>
+            </div>
+
+            <div id='Maintainability_description'>
+                <h2> Maintainability Description {review.Maintainability_rating}/100 </h2>
+                <p> {review.Maintainability_description} </p>
+            </div>
+
+
+        </div>
+    );
+
+}
+
+export default Review;
