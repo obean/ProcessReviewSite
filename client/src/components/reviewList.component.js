f98533c982ed3830d11e5d9884e24407c6710611
Johnny Yip <jyippy2012@gmail.com>
2020-11-18 16:25:23 +0000

    Styling review list in Profile

--- client/src/components/reviewList.component.js
+++ client/src/components/reviewList.component.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
+import './reviewList.component.css'
+
 
 function ReviewList(props) {
 
@@ -14,14 +16,14 @@ function ReviewList(props) {
   }
 
   return (
-    <div>
-      <h3>Your booked Reviews</h3> 
+    <div className="reviews">
+      <h3 className="review-header">Your booked Reviews</h3> 
        { props.reviews.map(review => (
       <li key={review.id}>
-        <Link to={`reviews/${review.id}`}>
+        <Link to={`reviews/${review.id}`} className="review-item">
           {review.booking_date}
           </Link>
-          <button onClick={() => cancelThis(review.id)}>Cancel</button>
+          <button className="cancel-button" onClick={() => cancelThis(review.id)}>Cancel</button>
          </li>
        ))}
        <Link to={'/book-review'}> Book a review</Link>
