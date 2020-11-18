bebeea437d6b87f0ea3ccd9ca5b8d699c79f5bb9
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-18 15:31:41 +0000

    First test passing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('sign in on landing page', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Sign in/i);
+  expect(linkElement).toBeInTheDocument();
+});
